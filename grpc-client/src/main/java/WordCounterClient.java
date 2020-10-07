2545db366ab83b80a2eb4f510270c13959bbc244
Gabriel Schaidhauer <gabriel@Gabriels-MacBook-Pro.local>
2020-10-07 20:31:03 -0300

    Adding other types of grpcs requests

--- grpc-client/src/main/java/WordCounterClient.java
+++ grpc-client/src/main/java/WordCounterClient.java
@@ -1,18 +1,23 @@
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import wordcounter.CountRequest;
-import wordcounter.CountResponse;
-import wordcounter.WordCounterServiceBaseGrpc;
+import io.grpc.stub.StreamObserver;
+import wordcounter.*;
+
+import java.util.Iterator;
 
 public class WordCounterClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String testString = "This is a string to test with 9 words";
+        String[] splitted = testString.split(" ");
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
                 .usePlaintext()
                 .build();
 
         WordCounterServiceBaseGrpc.WordCounterServiceBaseBlockingStub stub = WordCounterServiceBaseGrpc.newBlockingStub(channel);
+        WordCounterServiceBaseGrpc.WordCounterServiceBaseStub asyncStub = WordCounterServiceBaseGrpc.newStub(channel);
+
+        System.out.println("Count Words:");
         CountRequest request = CountRequest
                 .newBuilder()
                 .setMessage(testString)
@@ -20,6 +25,67 @@ public class WordCounterClient {
 
         CountResponse response = stub.countWords(request);
         System.out.println(response.getCount());
-    }
 
+        System.out.println("\nFilter words:");
+        FilterWordsRequest filterWordsRequest = FilterWordsRequest
+                .newBuilder()
+                .setMessage(testString)
+                .build();
+
+        Iterator<FilterWordsResponse> filterWordsResponseIterator = stub.filterWords(filterWordsRequest);
+        while (filterWordsResponseIterator.hasNext()) {
+            FilterWordsResponse filterWordsResponse = filterWordsResponseIterator.next();
+            System.out.println(filterWordsResponse.getWord());
+        }
+
+        System.out.println("\nBuild Message:");
+
+        StreamObserver<BuildMessageRequest> requestStreamObserver = asyncStub.buildMessage(new StreamObserver<BuildMessageResponse>() {
+            @Override
+            public void onNext(BuildMessageResponse value) {
+                System.out.println(value.getMessage());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                // Do nothing;
+            }
+        });
+
+        for (String word : splitted) {
+            requestStreamObserver.onNext(BuildMessageRequest.newBuilder().setWord(word).build());
+        }
+        requestStreamObserver.onCompleted();
+
+        Thread.sleep(500);
+
+        System.out.println("\nCount letters:");
+
+        StreamObserver<CountLettersRequest> countStreamObserver = asyncStub.countLetters(new StreamObserver<CountLettersResponse>() {
+            @Override
+            public void onNext(CountLettersResponse value) {
+                System.out.println(value.getWord() + " - " + value.getCount());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                // Do nothing
+            }
+        });
+
+        for (String word : splitted) {
+            countStreamObserver.onNext(CountLettersRequest.newBuilder().setWord(word).build());
+        }
+        countStreamObserver.onCompleted();
+    }
 }
--- grpc-server/src/main/java/com/whitepaper/wordcounter/WordCounterServiceImpl.java
+++ grpc-server/src/main/java/com/whitepaper/wordcounter/WordCounterServiceImpl.java
@@ -2,9 +2,7 @@ package com.whitepaper.wordcounter;
 
 
 import io.grpc.stub.StreamObserver;
-import wordcounter.CountRequest;
-import wordcounter.CountResponse;
-import wordcounter.WordCounterServiceBaseGrpc;
+import wordcounter.*;
 
 import java.util.Optional;
 
@@ -25,5 +23,66 @@ public class WordCounterServiceImpl extends WordCounterServiceBaseGrpc.WordCount
 
         responseObserver.onNext(reponse);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void filterWords(FilterWordsRequest request, StreamObserver<FilterWordsResponse> responseObserver) {
+        String[] words = Optional.ofNullable(request.getMessage())
+                .orElse("")
+                .split(" ");
+
+        for(String word : words) {
+            responseObserver.onNext(FilterWordsResponse.newBuilder().setWord(word).build());
+        }
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<BuildMessageRequest> buildMessage(StreamObserver<BuildMessageResponse> responseObserver) {
+        return new StreamObserver<BuildMessageRequest>() {
+            StringBuilder message = new StringBuilder();
+            @Override
+            public void onNext(BuildMessageRequest value) {
+                message.append(Optional.ofNullable(value.getWord()).orElse("")).append(" ");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(BuildMessageResponse.newBuilder().setMessage(message.toString()).build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    @Override
+    public StreamObserver<CountLettersRequest> countLetters(StreamObserver<CountLettersResponse> responseObserver) {
+        return new StreamObserver<CountLettersRequest>() {
+            @Override
+            public void onNext(CountLettersRequest value) {
+                String word = Optional.ofNullable(value.getWord()).orElse("");
+                int count = word.length();
+                responseObserver.onNext(CountLettersResponse
+                        .newBuilder()
+                        .setCount(count)
+                        .setWord(word)
+                        .build());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
